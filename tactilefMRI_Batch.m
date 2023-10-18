b723f3bc2d2bc4ae2a63d18e9f8d22113f8f5a95
lucyroe <lucy.roellecke@fu-berlin.de>
2023-10-18 17:12:21 +0200

    batch script updated

--- tactilefMRI_Batch.m
+++ tactilefMRI_Batch.m
@@ -1,53 +1,82 @@
+%% performs preprocessing & analysis of tactile perceptual bistability fMRI
+% fMRI data was obtained at the Neurocognitive Methods and Data Analysis
+% Practical class at Freie University Berlin on 16th and 17th October 2023
+
 %% SETUP -----------------------------------------------------------------
 
-n_runs = 6; % number of runs
-n_subj = 1; % number of subjects
-n_cond = 2; % number of conditions
+runs = {'1', '2', '3', '4', '5', '6'};  % runs
+subj = {'001'}; % subjects
+cond = {'simultaneous', 'alternating'}; % conditions
 
-steps = 1:4;    % analysis steps to be performed
-%   1:   Localizer Analysis
-%   2:   First Level Analysis - Contrasts Alternating / Simultaneous
-%   3:   Decoding
-%   4:   Group Level Analysis
+scriptpath = fullfile('/Users/Lucy/Documents/GitHub/NMDA_Practical');   
+% change to where the scripts are for you
+datapath = fullfile(['/Users/Lucy/Documents/Berlin/FU/MCNB/' ...
+    '3Semester/NMDA_Practical/data']);  % change to where data is for you
+spmpath = fullfile('/Users/Lucy/Documents/MATLAB/spm12');  
+% change to where you downloaded the spm toolbox
+addpath(scriptpath, datapath, spmpath)   % add script, data and spm path
 
-% *************************** DCM IMPORT *********************************
+steps = {'1', '2', '3', '4', '5'};    % analysis steps to be performed
+%   1:   Preprocessing
+%   2:   Localizer Analysis
+%   3:   First Level Analysis - Contrasts Alternating / Simultaneous
+%   4:   Decoding
+%   5:   Group Level Analysis
 
+spm('defaults', 'FMRI');    % setup spm
+spm_jobman('initcfg');  % initialize spm
 
-for step=1:len(steps)
-    %% STEP 1: LOCALIZER ANALYSIS ----------------------------------------
+% initialise fMRI analysis process loop over subjects
+for sub_no=1:len(subj)
+    sub = subj{sub_no};
+
+    % loop over the desired analysis steps
+    for step_no=1:len(steps)
+        step = steps{step_no};
+
+        if step == '1'
+        %% STEP 1: PREPROCESSING ------------------------------------------
+            % REALIGNMENT (motion correction) of epi images
+            % prefix "r"
+            
+            % CO-REGISTRATION puts t1 and (mean) epi image in the same 
+            % space
+            
+            % SEGMENTATION of the t1 image into white and grey matter
+            % output: deformation field
+            
+            % NORMALISATION uses deformation field to normalize epi images 
+            % into MNE space
+            % prefix "w"
+            
+            % SMOOTHING
+            % prefix "s"
+            
+        elseif step == '2' 
+        %% STEP 1: LOCALIZER ANALYSIS ------------------------------------
+            
+            % ********************** DATA FORMATTING *********************
+            % extract onsets of different conditions (alt/stim/baseline) 
+            % from localizer log file
+            
     
-    % ********************** DATA FORMATTING *****************************
-    % extract onsets of different conditions (alt/stim/baseline) from 
-    % localizer log file
+        elseif step == '3'
+        %% STEP 2: FIRST LEVEL ANALYSIS - CONTRASTS ALT/SIM --------------
+            
+            % ********************** DATA FORMATTING *********************
+            % extract onsets & durations of blocks / switches (from alt to 
+            % stim and vice versa) from run log file for each run
+
+            for run=1:n_runs
+            
+            end
     
-    % ************************ PREPROCESSING *****************************
-    % REALIGNMENT (motion correction) of epi images
-    % prefix "r"
-    
-    % CO-REGISTRATION puts t1 and (mean) epi image in the same space
-    
-    % SEGMENTATION of the t1 image into white and grey matter
-    % output: deformation field
-    
-    % NORMALISATION uses deformation field to normalize epi images into MNE
-    % space
-    % prefix "w"
-    
-    % SMOOTHING
-    % prefix "s"
-    
-    
-    %% STEP 2: FIRST LEVEL ANALYSIS - CONTRASTS ALT/SIM ------------------
-    
-    % ********************** DATA FORMATTING *****************************
-    % extract onsets & durations of blocks / switches (from alt to stim and
-    % vice versa) from run log file for each run
-    for run=1:n_runs
-    
+        elseif step == '4'
+        %% (STEP 3: DECODING) --------------------------------------------
+            
+        elseif step == '5'
+        %% (STEP 4: GROUP LEVEL ANALYSIS) --------------------------------
+
+        end
     end
-    
-    %% (STEP 3: DECODING) ------------------------------------------------
-    
-    
-    %% (STEP 4: GROUP LEVEL ANALYSIS) ------------------------------------
 end
